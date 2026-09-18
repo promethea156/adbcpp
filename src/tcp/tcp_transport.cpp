c72c0a336363cc264a8ff14b57dd803209c94d73
promethea156 <promethea156@gmail.com>
2026-09-18 16:42:39 +0300

    fix(tcp): include fcntl.h and netinet/tcp.h on POSIX

--- src/tcp/tcp_transport.cpp
+++ src/tcp/tcp_transport.cpp
@@ -12,7 +12,9 @@
 #    include <winsock2.h>
 #    include <ws2tcpip.h>
 #else
+#    include <fcntl.h>
 #    include <netdb.h>
+#    include <netinet/tcp.h>
 #    include <sys/select.h>
 #    include <sys/socket.h>
 #    include <sys/time.h>
